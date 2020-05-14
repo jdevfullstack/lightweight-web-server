3b39775b43c2e6d4fda9607ce0efc95705d7dbaf
xdvrx1 <mongAlvarez@gmail.com>
2020-05-14 14:14:24 +0800

    Update FileNotFoundMessage.java

--- src/xdvrx1_serverProject/FileNotFoundMessage.java
+++ src/xdvrx1_serverProject/FileNotFoundMessage.java
@@ -6,7 +6,7 @@ class FileNotFoundMessage  {
         new StringBuilder("<html>\r\n")
         .append("<head><title>File Not Found</title>\r\n")
         .append("</head>\r\n")
-        .append("<bodyString>")
+        .append("<body>")
         .append("<h1>HTTP Error 404: File Not Found [Try again later]</h1>\r\n")
         .append("</body></html>\r\n")
         .toString();
